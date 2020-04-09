0ea6678e41a9a35103a9011c236e15be9d2f6561
Scott Carpenter <sdcarpenter@gmail.com>
2020-04-09 02:37:07 +0000

    Setup SSL auto renewal

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+COMPOSE="/usr/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
 
 cd /home/ubuntu/src/crossridgehoa
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$COMPOSE run certbot renew && $COMPOSE kill -s SIGHUP webserver
 $DOCKER system prune -af
